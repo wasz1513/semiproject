88a8d135d1af65f8a217868604eb88c70cbe1e60
lkjhg56 <56664396+lkjhg56@users.noreply.github.com>
2019-12-17 14:21:11 +0900

    CustomerDao - getConnection();

--- semi/src/beans/CustomerDao.java
+++ semi/src/beans/CustomerDao.java
@@ -1,5 +1,24 @@
 package beans;
 
+import java.sql.Connection;
+
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
 public class CustomerDao {
 
+	private static DataSource source;
+	static {
+		try {
+			InitialContext ctx = new InitialContext();
+			source = (DataSource) ctx.lookup("java:comp/env/jdbc/oracle");
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public Connection getConnection() throws Exception{
+		return source.getConnection();
+	}
+	
 }
