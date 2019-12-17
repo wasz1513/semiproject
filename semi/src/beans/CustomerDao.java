aab9676026677ab23016a7f23aa2b7e9c1b754ed
lkjhg56 <56664396+lkjhg56@users.noreply.github.com>
2019-12-17 15:18:05 +0900

    Dao 이형주부분 완료

--- semi/src/beans/CustomerDao.java
+++ semi/src/beans/CustomerDao.java
@@ -1,12 +1,14 @@
 package beans;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 
 import javax.naming.InitialContext;
 import javax.sql.DataSource;
 
 public class CustomerDao {
-
+	
+	//DB 연결식
 	private static DataSource source;
 	static {
 		try {
@@ -17,8 +19,51 @@ public class CustomerDao {
 		}
 	}
 	
+	//getConnection
 	public Connection getConnection() throws Exception{
 		return source.getConnection();
 	}
 	
+	//회원 비밀번호 변경
+	public void ChangePassword(CustomerDto dto) throws Exception{
+		Connection con = getConnection();
+		String sql = "update customer set customer_pw=? where customer_id";
+		PreparedStatement ps = con.prepareStatement(sql);
+		ps.setString(1, dto.getCustomer_pw());
+		ps.setString(2, dto.getCustomer_id());
+		ps.execute();
+		con.close();
+	}
+	//회원 정보 변경(닉네임, 폰번호, post, 기본주소, 상세주소)
+	public void updateCustomerInfo(CustomerDto dto) throws Exception{
+		Connection con = getConnection();
+		String sql = "update customer set customer_nickname=?,customer_phone=?,customer_post=?,customer_basic_address=?,customer_extra_address=? where id=?";
+		PreparedStatement ps = con.prepareStatement(sql);
+		ps.setString(1, dto.getCustomer_nickname());
+		ps.setString(2, dto.getCustomer_phone());
+		ps.setString(3, dto.getCustomer_post());
+		ps.setString(4, dto.getCustomer_basic_address());
+		ps.setString(5, dto.getCustomer_extra_address());
+		ps.setString(6, dto.getCustomer_id());
+		ps.execute();
+		con.close();
+	}
+	//마지막 접속시간 변경
+	public void updateLastLogin(int customer_no) throws Exception{
+		Connection con = getConnection();
+		String sql = "update customer set customer_lastlogin=sysdate where customer_no=?";
+		PreparedStatement ps = con.prepareStatement(sql);
+		ps.setInt(1, customer_no);
+		ps.execute();
+		con.close();
+	}
+	//회원 탈퇴
+	public void withdrawal(int customer_no) throws Exception{
+		Connection con = getConnection();
+		String sql = "delete from customer where customer_no=?";
+		PreparedStatement ps = con.prepareStatement(sql);
+		ps.setInt(1, customer_no);
+		ps.execute();
+		con.close();
+	}
 }
