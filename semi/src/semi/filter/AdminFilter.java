33cbe16938025a55bce52baaa309c98a96856cbf
hyunjun <wasz1513@gmail.com>
2019-12-19 14:41:37 +0900

    관리자필터추가

---
+++ semi/src/semi/filter/AdminFilter.java
@@ -0,0 +1,31 @@
+package semi.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class AdminFilter implements Filter{
+	
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		HttpServletRequest req = (HttpServletRequest)request;
+		HttpServletResponse resp = (HttpServletResponse)response;
+		String grade = (String)req.getSession().getAttribute("customer_grade");
+		boolean admin = grade.equals("관리자");
+		if(admin) {
+			chain.doFilter(request, response);
+		}else {
+			resp.sendError(403);
+		}
+	}
+
+}
