4a987b9d7af424cbf775e391ec3590b4614463ec
areumi65 <lar_0605@naver.com>
2019-12-20 19:04:17 +0900

    +구매 작업중

---
+++ semi/src/semi/bean/BuyDao.java
@@ -0,0 +1,48 @@
+package semi.bean;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+public class BuyDao {
+	
+	private static DataSource source;
+	static {
+		try {
+			InitialContext ctx = new InitialContext();
+			source = (DataSource) ctx.lookup("java:comp/env/jdbc/oracle");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public Connection getConnection() throws Exception {
+		return source.getConnection();
+	}
+
+	//구매 신규배송 주소
+	public void buyNewAddress(BuyDto dto) throws Exception{
+		
+		Connection con = getConnection();
+		
+		String sql = "update buytest set buy_post=?, buy_basic_address=?,buy_extra_address=? where customer_no =?";
+		PreparedStatement ps = con.prepareStatement(sql);
+		ps.setString(1,dto.getBuy_post());
+		ps.setString(2, dto.getBuy_basic_address());
+		ps.setString(3, dto.getBuy_extra_address());
+		ps.setInt(4, dto.getCustomer_no());
+		
+	
+		
+		con.close();
+			
+	}
+
+
+	
+	
+	
+
+}
